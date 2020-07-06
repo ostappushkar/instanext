30fac3cb1b0caa2196661a16f52df904a991e2e4
Ostap Pushkar <pushkarostap@gmail.com>
2020-07-06 12:29:42 +0300

    Meta tag: initial scale

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,13 +1,20 @@
-import BottomNav from "../bottomNavigation";
-import Navbar from "../../components/navbar";
+import BottomNav from '../bottomNavigation'
+import Navbar from '../../components/navbar'
+import Head from 'next/head'
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1 user-scalable=0"
+        />
+      </Head>
       <Navbar />
       {children}
       <BottomNav />
     </>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
